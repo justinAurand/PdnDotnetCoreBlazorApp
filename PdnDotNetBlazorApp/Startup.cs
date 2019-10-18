7f07fdb6a17fe687c3c0854e9aff0aed624df418
Justin Aurand <justin.aurand@energage.com>
2019-10-18 11:10:42 -0400

    refactor code. add setup to readme

--- PdnDotNetBlazorApp/Startup.cs
+++ PdnDotNetBlazorApp/Startup.cs
@@ -1,26 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using PdnDotNetBlazorApp.Data;
-
 namespace PdnDotNetBlazorApp
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using PdnDotNetBlazorApp.Data;
+
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -47,9 +41,7 @@ namespace PdnDotNetBlazorApp
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapBlazorHub();
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Add Readme Content
+# Blazor
+
+## Setup
+
+1. Visual Studio 2019 v1.40+
+2. .NET Core v3.0.100+
